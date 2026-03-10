5df613009e51fb71b49db567a698c7e7aa286aff
Chinmay Banik <chinmaybanik51643@gmail.com>
2026-03-10 14:59:55 +0600

    LabTask-02

--- LabTask-02/script.js
+++ LabTask-02/script.js
@@ -1,6 +1,6 @@
 const cell = document.querySelectorAll('.cell');
 const statusText = document.getElementById('status');
-const reset = document.getElementById('restartBtn');
+const reset = document.getElementById('reset');
 
 let currentPlayer = 'X';
 let gameActive = true;
@@ -17,6 +17,8 @@ const winningConditions = [
     [0, 4, 8],
     [2, 4, 6]
 ];
+
+initializeGame();
 
 function initializeGame() {
     cell.forEach(cell => cell.addEventListener('click', cellclicked));
@@ -46,10 +48,51 @@ function changePlayer() {
 
 
 function checkWin() {
+    let roundWon = false;
+    for (let i = 0; i < winningConditions.length; i++) {
+        const winCondition = winningConditions[i];
+        const a = board[winCondition[0]];
+        const b = board[winCondition[1]];
+        const c = board[winCondition[2]];
+        if (a === '' || b === '' || c === '') {
+            continue;
+        }
+        if (a === b && b === c) {
+            roundWon = true;
 
+            const index1 = winCondition[0];
+            const index2 = winCondition[1];
+            const index3 = winCondition[2];
+
+            cell[index1].classList.add('win');
+            cell[index2].classList.add('win');
+            cell[index3].classList.add('win');
+
+            break;
+        }
+    }
+    if (roundWon) {
+        statusText.textContent = `Player ${currentPlayer} wins!`;
+        gameActive = false;
+        return;
+    }
+    else if (!board.includes("")) {
+        statusText.textContent = `Game ended in a draw!`;
+        gameActive = false;
+        return;
+    }
+    else {
+        changePlayer();
+    }
 }
-
-
 function resetGame() {
+    currentPlayer = 'X';
+    gameActive = true;
+    board = ["", "", "", "", "", "", "", "", ""];
+    cell.forEach(cell => {
+        cell.textContent = "";
+        cell.classList.remove("win");
+    });
+    statusText.textContent = `Player ${currentPlayer}'s turn`;
 
 }
